005bce96369a642eb064462c4eaca8fbda7e6774
SharkFinStew <kajiquiet@gmail.com>
2024-02-03 12:49:53 -0400

    Test event listener

---
+++ src/main/java/org/example/AniListQuery.java
@@ -0,0 +1,43 @@
+package org.example;
+
+public class AniListQuery
+{
+    public String getActivityQuery()
+    {
+        String activityQuery = """
+                query ($search: Int) {
+                    Activity (userId: $search, sort: ID_DESC) {
+                        __typename
+                        ... on ListActivity {
+                            createdAt
+                            status
+                            media {
+                                id
+                                title {
+                                    english
+                                }
+                            }
+                            user {
+                                name
+                                avatar {
+                                    large
+                                    medium
+                                }
+                            }
+                        }
+                    }
+                }
+                """;
+        
+        return sanitizeQuery(activityQuery);
+    }
+    
+    
+    private String sanitizeQuery(String query)
+    {
+        query = query.replace('\n', ' ');
+        
+        return query;
+    }
+    
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
 import com.google.gson.Gson;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.example.Json.Activity;
 import org.example.Json.JsonResponse;
 
@@ -10,83 +13,87 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main
 {
-    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException
+    public static void main(String[] args) throws IOException
     {
-        // Here we define our query as a multi-line string
-        String query = """
-        query ($search: Int) {
-            Activity (userId: $search, sort: ID_DESC) {
-                __typename
-                ... on ListActivity {
-                    createdAt
-                    status
-                    media {
-                        id
-                        title {
-                            english
-                        }
-                    }
-                    user {
-                        name
-                        avatar {
-                            large
-                            medium
-                        }
-                    }
-                }
-            }
+        String token = Files.readString(Path.of("token.txt"));
+        
+        JDA jda = JDABuilder.createDefault(token)
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .addEventListeners(new MyListener())
+                .build();
+        
+        // retrieveActivityData();
+    }
+    
+    
+    private static void retrieveActivityData() throws IOException, URISyntaxException, InterruptedException
+    {
+        AniListQuery query = new AniListQuery();
+        
+        List<Integer> userIDs = Files.readAllLines(Path.of("IDs.txt"))
+                .stream()
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .toList();
+        
+        
+        for (int userID : userIDs)
+        {
+            Map<String, Object> variables = new HashMap<>();
+            variables.put("search", userID);
+            
+            
+            Map<String, Object> payload = new HashMap<>();
+            payload.put("query", query.getActivityQuery());
+            payload.put("variables", variables);
+            
+            
+            Gson gson = new Gson();
+            String json = gson.toJson(payload);
+            
+            
+            // Make the HTTP Api request
+            HttpClient client = HttpClient.newBuilder()
+                    .build();
+            
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("https://graphql.anilist.co"))
+                    .header("Content-Type", "application/json")
+                    .header("Accept", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(json))
+                    .build();
+            
+            
+            // Receive the HTTP response
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            
+            
+            // Parse Json Response
+            JsonResponse jsonResponse = gson.fromJson(response.body(), JsonResponse.class);
+            
+            
+            // Print out the information we want
+            Activity activity = jsonResponse.getData().getActivity();
+            
+            System.out.println(activity.getCreatedAt());
+            System.out.println(activity.getUser().getName());
+            System.out.println(activity.getUser().getAvatar().getMedium());
+            System.out.println(activity.getStatus());
+            System.out.println(activity.getMedia().getTitle().getEnglish());
+            System.out.println(activity.getMedia().getId());
+            
+            System.out.println();
+            System.out.println();
+            
+            Thread.sleep(10000 / userIDs.size());
         }
-        """;
-        query = query.replace('\n', ' ');
-        
-        
-        // Define our query variables and values that will be used in the query request
-        int userID = 0;
-        
-        Map<String, Object> variables = new HashMap<>();
-        
-        variables.put("search", userID);
-        
-        
-        Map<String, Object> payload = new HashMap<>();
-        
-        payload.put("query", query);
-        payload.put("variables", variables);
-        
-        
-        Gson gson = new Gson();
-        String json = gson.toJson(payload);
-        
-        
-        // Make the HTTP Api request
-        HttpClient client = HttpClient.newBuilder()
-                .build();
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("https://graphql.anilist.co"))
-                .header("Content-Type", "application/json")
-                .header("Accept", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(json))
-                .build();
-
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        
-        System.out.println(response.body());
-        
-        JsonResponse jsonResponse = gson.fromJson(response.body(), JsonResponse.class);
-        
-        Activity activity = jsonResponse.getData().getActivity();
-        
-        System.out.println(activity.getCreatedAt());
-        System.out.println(activity.getUser().getName());
-        System.out.println(activity.getUser().getAvatar().getMedium());
-        System.out.println(activity.getStatus());
-        System.out.println(activity.getMedia().getTitle().getEnglish());
-        System.out.println(activity.getMedia().getId());
     }
 }
